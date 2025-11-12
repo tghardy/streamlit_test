4105943202e48fb6b30457585c7f9f5f13ba907a
Timothy Hardy <timothyhardyg@gmail.com>
2025-11-12 13:36:16 -0700

    test app

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+
+df = pd.read_csv("byu_football_stats_2025.csv")
+
+st.title("BYU Football- Overrated?")
+st.line_chart(df, y=["thirdDownEff", "fourthDownEff"])
+
+df["possessionTime"] = df["possessionTime"].apply(lambda x: x/60)
+st.markdown("## What about their posession time?")
+st.line_chart(df, y=["possessionTime", "opponent_score", "byu_score"])
